91a190ed4cd92d307866a00d13b8d7f58d663838
mike22meko <mike22meko@gmail.com>
2011-11-04 05:26:43 +0000

    class, method docstrings

--- Node.py
+++ Node.py
@@ -8,24 +8,25 @@
 
 from Constants import *
 
-"""
-Representation for a node in a graph
-A graph is represented by a dictionary mapping node id's to |Node| objects
-"""
 class Node:
+  """
+  Representation for a node in a graph
+  A graph is represented by a dictionary mapping node id's to |Node| objects
+  """
+
   def __init__(self):
     # Neighboring nodes
     setattr(self, NEIGHBORS, set())
     # Weight of the node
     setattr(self, WEIGHT, 1.0)
 
-  """
-  Add a neighbor to this node
-  |neighbor_id|: id of the neighboring node
-  |edge_weight|: weight of the edge connecting the two nodes
-  |accumulation_weights|: weight of the edge based on other metrics
-  """
   def add_neighbor(self, neighbor_id, edge_weight=1.0, accumulation_weights={}):
+    """
+    Add a neighbor to this node
+    |neighbor_id|: id of the neighboring node
+    |edge_weight|: weight of the edge connecting the two nodes
+    |accumulation_weights|: weight of the edge based on other metrics
+    """
     getattr(self, NEIGHBORS).add((neighbor_id,
                                   edge_weight,
                                   tuple(accumulation_weights.items())))
